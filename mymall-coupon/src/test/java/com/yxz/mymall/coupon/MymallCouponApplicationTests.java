c0bf158f6a20e2c3bd3951fbae5a47e8e400c167
xinze <xinzeyu@seu.edu.cn>
2022-04-12 15:50:52 +0800

    项目结构创建！

---
+++ mymall-coupon/src/test/java/com/yxz/mymall/coupon/MymallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yxz.mymall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
