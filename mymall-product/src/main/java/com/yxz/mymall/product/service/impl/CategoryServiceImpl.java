3d95b2e80d94d93241b77802dab13cf147494c60
xinze <nuaa_yxz@163.com>
2022-07-07 18:49:56 +0800

    redis与分布式锁的整合使用！

--- mymall-product/src/main/java/com/yxz/mymall/product/service/impl/CategoryServiceImpl.java
+++ mymall-product/src/main/java/com/yxz/mymall/product/service/impl/CategoryServiceImpl.java
@@ -1,12 +1,21 @@
 package com.yxz.mymall.product.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.yxz.mymall.product.service.CategoryBrandRelationService;
 import com.yxz.mymall.product.vo.Catelog2Vo;
 import org.apache.ibatis.ognl.CollectionElementsAccessor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -27,6 +36,12 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
     @Autowired
     private CategoryBrandRelationService categoryBrandRelationService;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedissonClient redisson;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -110,28 +125,127 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         }
     }
 
+    /*
+
+        自定义：
+        1） 指定使用的key：key属性指定
+        2） 指定缓存数据的存活时间:配置文件中指定
+        3） 将数据保存为json格式
+     */
+    @Cacheable(value = {"category"}, key = "#root.method.name")  //代表当前结果需要缓存
     @Override
     public List<CategoryEntity> getLevel1Categorys() {
         List<CategoryEntity> entityList = baseMapper.selectList(new QueryWrapper<CategoryEntity>().eq("parent_cid", 0));
         return entityList;
     }
 
+    //TODO 产生堆外内存溢出
+
     @Override
     public Map<String, List<Catelog2Vo>> getCatalogJson() {
+
+        /**
+         * 1 空结果缓存，解决缓存穿透问题
+         * 2 设置过期时间，随机值，解决缓存雪崩问题
+         * 3 加锁，解决缓存击穿问题
+         */
+
+        //缓存中的对象时JSON字符串
+        String catalogJSON = stringRedisTemplate.opsForValue().get("catalogJSON");
+        if (StringUtils.isEmpty(catalogJSON)) {
+            //缓存中没有，查数据库
+            Map<String, List<Catelog2Vo>> catalogJsonFromDb = getCatalogJsonFromDbWithRedissonLock();
+
+            return catalogJsonFromDb;
+        }
+
+        Map<String, List<Catelog2Vo>> result = JSON.parseObject(catalogJSON, new TypeReference<Map<String, List<Catelog2Vo>>>(){});
+        return result;
+    }
+
+    public Map<String, List<Catelog2Vo>> getCatalogJsonFromDbWithRedissonLock() {
+
+        //1. 占分布式锁，锁的粒度选择，具体缓存的是某个数据，11号商品：product-11-lock
+        RLock lock = redisson.getLock("catalogJson-lock");
+        lock.lock();
+        System.out.println("获取分布式锁成功。。");
+
+        Map<String, List<Catelog2Vo>> dataFromDb;
+        try {
+            dataFromDb = getDataFromDb();
+        } finally {
+            lock.unlock();
+        }
+        return dataFromDb;
+    }
+
+    public Map<String, List<Catelog2Vo>> getCatalogJsonFromDbWithRedisLock() {
+
+        //1. 占分布式锁
+        String uuid = UUID.randomUUID().toString();
+        Boolean lock = stringRedisTemplate.opsForValue().setIfAbsent("lock", uuid, 300, TimeUnit.SECONDS);
+        if (lock) {
+            System.out.println("获取分布式锁成功。。");
+            //加锁成功，执行业务
+            //设置过期时间，避免解锁失败，需要和加锁是原子性的
+            //stringRedisTemplate.expire("lock", 30, TimeUnit.SECONDS);
+            Map<String, List<Catelog2Vo>> dataFromDb;
+            try {
+                dataFromDb = getDataFromDb();
+            } finally {
+                String lua_scripts = "if redis.call('get',KEYS[1]) == ARGV[1] then " +
+                        "return redis.call('del',KEYS[1]) else return 0 end";
+                //解锁保证原子性
+                stringRedisTemplate.execute(new DefaultRedisScript<Integer>(lua_scripts, Integer.class), Arrays.asList("lock")
+                        , uuid);
+            }
+
+            //获取值对比+对比成功删除也应当做成原子操作，lua脚本解锁
+            /*String lockValue = stringRedisTemplate.opsForValue().get("lock");
+            if (uuid.equals(lockValue)) {
+                stringRedisTemplate.delete("lock"); //删除锁
+            }*/
+            return dataFromDb;
+        } else {
+            //等待重试
+            //可以设定休眠时间
+            System.out.println("获取分布式锁失败。。");
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return getCatalogJsonFromDbWithRedisLock(); //自旋
+        }
+    }
+
+    private Map<String, List<Catelog2Vo>> getDataFromDb() {
+        //得到锁之后，再去缓存中确定一次
+        String catalogJSON = stringRedisTemplate.opsForValue().get("catalogJSON");
+        if (!StringUtils.isEmpty(catalogJSON)) {
+            Map<String, List<Catelog2Vo>> result = JSON.parseObject(catalogJSON, new TypeReference<Map<String, List<Catelog2Vo>>>(){});
+            return result;
+        }
+        System.out.println("查询了数据库。。。。");
+
+        //否则继续进行下一步
+        List<CategoryEntity> selectList = baseMapper.selectList(null);
+
+
         //查出所有一级分类
-        List<CategoryEntity> level1Categorys = getLevel1Categorys();
+        List<CategoryEntity> level1Categorys = getParent_cid(selectList, 0L);
 
         //封装数据
         Map<String, List<Catelog2Vo>> parent_cid = level1Categorys.stream().collect(Collectors.toMap(k -> k.getCatId().toString(), v -> {
             //每一个一级分类，查到这个一级分类的二级分类
-            List<CategoryEntity> entityList = baseMapper.selectList(new QueryWrapper<CategoryEntity>().eq("parent_cid", v.getCatId()));
+            List<CategoryEntity> entityList = getParent_cid(selectList, v.getCatId());
             //封装上面的结果
             List<Catelog2Vo> catelog2Vos = null;
             if (entityList != null) {
                 catelog2Vos = entityList.stream().map(l2 -> {
                     Catelog2Vo catelog2Vo = new Catelog2Vo(v.getCatId().toString(), null, l2.getCatId().toString(), l2.getName());
                     //找当前2级分类的3级分类
-                    List<CategoryEntity> level3Catelog = baseMapper.selectList(new QueryWrapper<CategoryEntity>().eq("parent_cid", l2.getCatId()));
+                    List<CategoryEntity> level3Catelog = getParent_cid(selectList, l2.getCatId());
                     if (level3Catelog != null) {
                         List<Catelog2Vo.Category3Vo> category3Vos = level3Catelog.stream().map(l3 -> {
                             Catelog2Vo.Category3Vo category3Vo = new Catelog2Vo.Category3Vo(l2.getCatId().toString(), l3.getCatId().toString(), l3.getName());
@@ -146,7 +260,75 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
             return catelog2Vos;
         }));
 
+        String s = JSON.toJSONString(parent_cid);
+        stringRedisTemplate.opsForValue().set("catalogJSON", s, 1, TimeUnit.DAYS);
         return parent_cid;
+    }
+
+    /**
+     *
+     * 本地锁
+     * @return
+     */
+    public Map<String, List<Catelog2Vo>> getCatalogJsonFromDbWithLocalLock() {
+
+        //如何加锁
+        //1、synchronized(this):this是单例的
+        //2、方法上加synchronized
+        //TODO 本地锁：synchronized，lock，在分布式情况下想要锁住所有必须使用分布式锁
+
+        synchronized (this) {
+            //得到锁之后，再去缓存中确定一次
+            String catalogJSON = stringRedisTemplate.opsForValue().get("catalogJSON");
+            if (!StringUtils.isEmpty(catalogJSON)) {
+                Map<String, List<Catelog2Vo>> result = JSON.parseObject(catalogJSON, new TypeReference<Map<String, List<Catelog2Vo>>>(){});
+                return result;
+            }
+
+            //否则继续进行下一步
+            List<CategoryEntity> selectList = baseMapper.selectList(null);
+
+
+            //查出所有一级分类
+            List<CategoryEntity> level1Categorys = getParent_cid(selectList, 0L);
+
+            //封装数据
+            Map<String, List<Catelog2Vo>> parent_cid = level1Categorys.stream().collect(Collectors.toMap(k -> k.getCatId().toString(), v -> {
+                //每一个一级分类，查到这个一级分类的二级分类
+                List<CategoryEntity> entityList = getParent_cid(selectList, v.getCatId());
+                //封装上面的结果
+                List<Catelog2Vo> catelog2Vos = null;
+                if (entityList != null) {
+                    catelog2Vos = entityList.stream().map(l2 -> {
+                        Catelog2Vo catelog2Vo = new Catelog2Vo(v.getCatId().toString(), null, l2.getCatId().toString(), l2.getName());
+                        //找当前2级分类的3级分类
+                        List<CategoryEntity> level3Catelog = getParent_cid(selectList, l2.getCatId());
+                        if (level3Catelog != null) {
+                            List<Catelog2Vo.Category3Vo> category3Vos = level3Catelog.stream().map(l3 -> {
+                                Catelog2Vo.Category3Vo category3Vo = new Catelog2Vo.Category3Vo(l2.getCatId().toString(), l3.getCatId().toString(), l3.getName());
+                                return category3Vo;
+                            }).collect(Collectors.toList());
+                            catelog2Vo.setCatalog3List(category3Vos);
+                        }
+
+                        return catelog2Vo;
+                    }).collect(Collectors.toList());
+                }
+                return catelog2Vos;
+            }));
+
+            String s = JSON.toJSONString(parent_cid);
+            stringRedisTemplate.opsForValue().set("catalogJSON", s, 1, TimeUnit.DAYS);
+            return parent_cid;
+        }
+    }
+
+    private List<CategoryEntity> getParent_cid(List<CategoryEntity >selectList, Long parent_cid) {
+        //return baseMapper.selectList(new QueryWrapper<CategoryEntity>().eq("parent_cid", v.getCatId()));
+        List<CategoryEntity> collect = selectList.stream().filter(item -> {
+            return item.getParentCid() == parent_cid;
+        }).collect(Collectors.toList());
+        return collect;
     }
 
     private void findParentPath(Long catelogId, List<Long> path) {
--- mymall-product/src/main/java/com/yxz/mymall/product/web/IndexController.java
+++ mymall-product/src/main/java/com/yxz/mymall/product/web/IndexController.java
@@ -3,6 +3,8 @@ package com.yxz.mymall.product.web;
 import com.yxz.mymall.product.entity.CategoryEntity;
 import com.yxz.mymall.product.service.CategoryService;
 import com.yxz.mymall.product.vo.Catelog2Vo;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,12 +13,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class IndexController {
 
     @Autowired
     CategoryService categoryService;
+
+    @Autowired
+    RedissonClient redissonClient;
 
     @GetMapping({"/", "/index.html"})
     public String indexPage(Model model) {
@@ -38,4 +44,36 @@ public class IndexController {
         return catalogJson;
 
     }
+
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        //获取一把锁，只要锁的名字一样，就是同一把锁
+        RLock lock = redissonClient.getLock("my_lock");
+
+        //加锁，阻塞式等待，有自动续期机制
+        //lock.lock();
+        lock.lock(10, TimeUnit.SECONDS); //在锁时间到了以后不会自动续期，自动解锁时间一定要大于业务的执行时间
+
+        //最佳实战
+        /**
+         * 1. lock.lock(10, TimeUnit.SECONDS); 省掉续期操作，手动解锁
+         * 2. 读写锁。读锁是共享锁，写锁是排他锁，互斥锁。
+         * 3. 闭锁。计数解锁。
+         * 4. 信号量 tryAcquire()，无需阻塞式等待，可以用作限流
+         */
+        try{
+            System.out.println("枷锁成功，执行。。");
+            Thread.sleep(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+        return "hello";
+
+    }
+
+
+
 }
